c8fd52cba1b413edc4afc939b250ed80c8a4620d
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-04-05 20:35:41 +0530

    Blog complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Blog from "./Blog/Blog";
 import Dashboard from "./Dashboard/Dashboard";
 import Error from "./Error/Error";
 import Home from "./Home/Home";
@@ -14,6 +15,7 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/reviews" element={<Reviews></Reviews>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blog></Blog>}></Route>
         <Route path="*" element={<Error></Error>}></Route>
       </Routes>
     </div>
